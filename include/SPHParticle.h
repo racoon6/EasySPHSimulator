b8ddd8abe0da3b4f89aaf73e9875121231a462df
racoon6 <15602193862@163.com>
2025-12-18 00:48:56 +0800

    PUSH SPHParticle.h

---
+++ include/SPHParticle.h
@@ -0,0 +1,33 @@
+//
+// Created by Wanghuanxiong on 2025/12/17.
+//
+
+#ifndef SPHPARTICLE_H
+#define SPHPARTICLE_H
+#include "glm/glm.hpp"
+
+// SPH粒子类（WCSPH：弱可压缩SPH）
+struct SPHParticle
+{
+    glm::vec3 pos;//粒子位置
+    glm::vec3 vel;//粒子速度
+    glm::vec3 acc;//加速度
+    float density;//密度
+    float pressure;//压力
+    float mass = 0.02f;//质量（取固定值）
+
+    // 重置加速度（每次仿真步长前）
+    constexpr void  resetAcc() noexcept
+    {
+        acc = glm::vec3(0.0f);
+    }
+
+    constexpr void integrate(float dt) noexcept
+    {
+        vel += dt * acc;
+        pos += dt * vel;
+        // 可选：速度阻尼（防止数值爆炸）
+        vel *= vel * 0.999f;
+    }
+};
+#endif //SPHPARTICLE_H
